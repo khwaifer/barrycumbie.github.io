03981d52491950e87fd782beb168ace495dd3332
khwaifer <37086953+khwaifer@users.noreply.github.com>
2018-03-07 16:04:29 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-$(document).ready(function(){
+$(document).ready(function){
 	var nodeList = document.body.childNodes;
 		for (i=0; i < nodeList.length; i++) {
 			console.log(document.body.childNodes[i]);
